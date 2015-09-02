af7a9ff03b446c25ef940975b5db1f5487a32c07
unknown-user <dennis.seiffert@web.de>
2015-09-02 16:58:00 +0200

    remove comments

--- ShoppingCart/NewLineClassifier.cs
+++ ShoppingCart/NewLineClassifier.cs
@@ -13,19 +13,8 @@ namespace ShoppingCart
 		#region ICharacterMatching implementation
 
 		char ICharacterMatching.Recognize (Sample sample)
-		{	
-//			var amountOfBrighterPixels = sample.Values.Where (v => v > 0.6).Count ();
-//			var amountOfDarkerPixels = sample.Values.Where (v => v < 0.2).Count ();
-
-
-			//var variance = sample.Values.Variance (false);
+		{				
 			var deviation = sample.Values.StandardDeviation (false);
-			//var mean = sample.Values.Mean ();
-			//var entropy = sample.Values.Entropy ();
-
-			// Console.Out.WriteLine ("Variance: {0}, Deviation: {1}, mean: {2}, entropy: {3}", variance, deviation, mean, entropy); 
-
-			//	if (amountOfBrighterPixels > 0 && (amountOfDarkerPixels / amountOfBrighterPixels) < 0.1) {
 			if (deviation < 0.1) {
 				return '\n';
 			}
