d369ef000d5fdcb2a763e05ce2ff7cf505e6fb9e
unknown-user <dennis.seiffert@web.de>
2015-09-03 16:13:15 +0200

    write test to verify block segmentation

---
+++ ShoppingCartTests/BlockSegmentationTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using ShoppingCart;
+using System.Collections.Generic;
+using NSubstitute;
+using System.Linq;
+using Should;
+
+namespace ShoppingCartTests
+{
+	[TestFixture]
+	public class BlockSegmentationTests
+	{
+		public BlockSegmentationTests ()
+		{
+		}
+
+		[Test]
+		public void ShouldCreateBlocksFromLine ()
+		{
+			var line = new List<Sample> ();
+			line.Add (new Sample (new double[]{ 1.0, 0.0, 1.0 }, ' ', 1.0));
+			line.Add (new Sample (new double[]{ 1.0, 0.0, 1.0 }, ' ', 1.0));
+			var blanklineMatching = Substitute.For<ICharacterMatching> ();
+			blanklineMatching.Recognize (Arg.Any<Sample> ()).Returns ((NSubstitute.Core.CallInfo arg) => {
+				var sample = arg.Arg<Sample> ();
+				if (sample.Values.Sum () == 0)
+					return '|';
+				return ' ';
+			});
+			var sut = new BlockSegmentation (blanklineMatching);
+
+			var result = sut.Segment (line);
+
+			result.Count ().ShouldEqual (2);
+			result.First ().Values.Sum ().ShouldEqual (2.0);
+			result.Last ().Values.Sum ().ShouldEqual (2.0);
+		}
+	}
+}
+
