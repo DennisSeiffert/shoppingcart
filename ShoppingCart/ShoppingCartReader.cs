fabc6b691129a74f879860605376d09c47da6acb
unknown-user <dennis.seiffert@web.de>
2015-09-03 16:20:20 +0200

    include blocksegmentation class into ShoppingCartReader

--- ShoppingCart/ShoppingCartReader.cs
+++ ShoppingCart/ShoppingCartReader.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Drawing;
 using Accord.Math;
 using System.Threading.Tasks;
+using System.Text;
 
 namespace ShoppingCart
 {
@@ -14,13 +15,13 @@ namespace ShoppingCart
 
 		private LineSegmentation lineSegmentation;
 
-		private ICharacterMatching blankLineClassifier;
+		private BlockSegmentation blockSegmentation;
 
 		public ShoppingCartReader (ICharacterMatching digitClassifier, ICharacterMatching newLineClassifier, 
 		                           ICharacterMatching blankLineClassifier)
-		{
-			this.blankLineClassifier = blankLineClassifier;
+		{			
 			this.lineSegmentation = new LineSegmentation (newLineClassifier);
+			this.blockSegmentation = new BlockSegmentation (blankLineClassifier);
 			this.digitClassifier = digitClassifier;
 		}
 
@@ -32,48 +33,17 @@ namespace ShoppingCart
 		public string Read (IEnumerable<Sample> imageRows)
 		{			
 			var lines = this.lineSegmentation.Segment (imageRows).ToList ();
+			var readShoppingCart = new StringBuilder ();
 
 			foreach (var line in lines) {
-				int leftBorder = 0;
-				int rightBorder = 0;
-				double[,] matrix = Matrix.Create (line.Count, 1, 0.0);
-				for (int i = 0; i < line.First ().Values.Length; i++) {
-					var verticalLine = line.Select (s => s.Values [i]).ToArray ();
-					matrix.InsertColumn (verticalLine);
-					var character = this.blankLineClassifier.Recognize (new Sample (verticalLine, 1.0));
-					if (character == '|') {
-						rightBorder = i;
-						int height = line.Count, width = rightBorder - leftBorder;
-						int segmentationWindowWidth = (int)Math.Ceiling (width / 8.0), segmentationWindowHeight = (int)Math.Ceiling (height / 8.0);
-
-						double[] characterIntensityDistribution = new double[64];
-						int quadrant = 0;
-						double maxValue = 0;
-						for (int stepY = 0; stepY < width; stepY += segmentationWindowHeight) {							
-							for (int stepX = 0; stepX < width; stepX += segmentationWindowWidth) {
-								var activatedPixels = 0.0;
-								var subMatrix = matrix.Submatrix (stepY, stepY + segmentationWindowHeight, stepX, stepX + segmentationWindowWidth);
-								subMatrix.Apply (e => activatedPixels += e);
-								characterIntensityDistribution [quadrant] = activatedPixels;
-								maxValue = Math.Max (maxValue, activatedPixels);
-
-								quadrant++;
-							}	
-						}
-
-						matrix = Matrix.Create (line.Count, 1, 0.0);
-
-						char digit = this.digitClassifier.Recognize (new Sample (characterIntensityDistribution, maxValue));
-
-						Console.Out.Write (digit);
-
-						leftBorder = i;
-					}
-				}
-
+				foreach (var block in this.blockSegmentation.Segment (line)) {
+					char digit = this.digitClassifier.Recognize (block);
+					readShoppingCart.Append (digit);
+				} 					
+				readShoppingCart.AppendLine ();
 			}
 
-			return string.Empty;
+			return readShoppingCart.ToString ();
 		}
 
 
