f08b85f700ca17bbdc833cd1a514f2ce92821d03
unknown-user <dennis.seiffert@web.de>
2015-09-10 22:19:13 +0200

    create test to verify voucher parsing with simple voucher

---
+++ ShoppingCart/Voucher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Antlr4.Runtime;
+using System.Linq;
+
+namespace ShoppingCart
+{
+	public class Voucher : voucherBaseListener
+	{
+		public Voucher (string voucherText)
+		{	
+			var lexer = new voucherLexer (new AntlrInputStream (voucherText));
+			CommonTokenStream tokens = new CommonTokenStream (lexer);
+			var parser = new voucherParser (tokens);
+			parser.AddParseListener (this);
+
+			parser.voucher ();
+		}
+
+		public override void ExitLine (voucherParser.LineContext context)
+		{
+			base.ExitLine (context);
+
+			bool isTotalSum = context.children.Any (c => c.GetText () == "Summe");
+			if (isTotalSum) {
+				this.Total = double.Parse (context.price ().GetText ());
+			}
+		}
+
+		public double Total { get; private set; }
+	}
+}
+
---
+++ ShoppingCartTests/VoucherTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using ShoppingCart;
+using Should;
+
+namespace ShoppingCartTests
+{
+	[TestFixture]
+	public class VoucherTests
+	{
+		[Test]
+		public void ShouldParseVoucherFromString ()
+		{
+			var sut = new Voucher ("Summe 5.67");
+
+			sut.Total.ShouldEqual (5.67);
+		}
+	}
+}
+
