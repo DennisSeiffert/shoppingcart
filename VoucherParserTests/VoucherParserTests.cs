5337a934e59c022de1ddde363d053a6db2dd684b
unknown-user <dennis.seiffert@web.de>
2015-09-10 21:56:05 +0200

    integrates simple voucher grammar in c# generated by antlr4.5

---
+++ VoucherParserTests/VoucherParserTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using Antlr4.Runtime;
+
+namespace VoucherParserTests
+{
+	[TestFixture ()]
+	public class VoucherParserTests
+	{
+		[Test ()]
+		public void ShouldParseSimpleVoucher ()
+		{
+			var lexer = new voucherLexer (new AntlrInputStream ("Summe 4.56"));
+			CommonTokenStream tokens = new CommonTokenStream (lexer);
+			var parser = new voucherParser (tokens);
+			var listener = new voucherBaseListener ();
+			parser.AddParseListener (listener);
+
+			var context = parser.voucher ();
+		}
+	}
+}
+
